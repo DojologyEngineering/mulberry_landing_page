e91ae4b78ed1038bc93a39cea026109831d4c9e9
MyMac <mymac@MyMacs-MacBook-Pro.local>
2024-02-27 17:04:01 +0700

    add-contact

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ContactUs from '@/components/ContactUs';
+
+function page() {
+  return (
+    <div>
+      <ContactUs />
+    </div>
+  );
+}
+
+export default page;
