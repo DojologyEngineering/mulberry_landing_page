ca06280939e7caa7369108496834fdb4d9bb90d8
MyMac <mymac@MyMacs-MacBook-Pro.local>
2024-05-25 18:46:51 +0700

    fix-meta

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from 'next';
 import React from 'react';
 
 import ContactUs from '@/components/ContactUs';
 
+export const metadata: Metadata = {
+  title: ' Contact Us | Mulberry Learning Cambodia',
+  description:
+    'Mulberry Learning Cambodia is a vibrant environment that promotes holistic learning and empowers children to become enthusiastic, confident, and caring global citizens of the world. Powered by Cambodia Award-winning Reggio-inspired Preschool, certified by the USA`s Habits of Mind Institute',
+};
 function page() {
   return (
     <div>
