1efdb574f9efa0fadeaf7e90667745c19ca80414
BrandoWeaver <srunrotha614@gmail.com>
2024-08-13 13:01:48 +0700

    fix transition

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -10,22 +10,19 @@ import { images, mediumScreen, smallScreen } from '@/utils/home-util';
 
 import image1 from '/public/img/image1.webp';
 
-// export const ImagePreview = () => {
-//   return (
-//     <div className='container w-full mx-auto mt-10 mb-10'>
-//       <ImageGallery
-//         items={images}
-//         infinite={false}
-//         additionalClass='bg-white'
-//       />
-//     </div>
-//   );
-// };
+export const ImagePreview = () => {
+  return (
+    <div className='container w-full mx-auto mt-10 mb-10'>
+      <ImageGallery
+        items={images}
+        infinite={false}
+        additionalClass='bg-white'
+      />
+    </div>
+  );
+};
 export const HomePreview = () => {
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
-  const handleSlide = (index: number) => {
-    setActiveSlideIndex(index);
-  };
   const [fontSize, setFontSize] = useState('text-[26px]');
   useEffect(() => {
     const handleResize = () => {
@@ -53,11 +50,13 @@ export const HomePreview = () => {
             items={smallScreen}
             infinite={true}
             // autoPlay={true}
+            slideDuration={400}
+            // showIndex
             showThumbnails={false}
             showFullscreenButton={false}
             showPlayButton={false}
             onSlide={(index) => {
-              handleSlide(index);
+              setActiveSlideIndex(index);
             }}
             onBeforeSlide={(newIndex) => {
               setActiveSlideIndex(newIndex);
@@ -69,76 +68,71 @@ export const HomePreview = () => {
             items={mediumScreen}
             additionalClass='w-full'
             infinite={true}
-            autoPlay={true}
+            // autoPlay={true}
             showThumbnails={false}
+            // showIndex
+            slideDuration={400}
             showFullscreenButton={false}
             showPlayButton={false}
             onSlide={(index) => {
-              handleSlide(index);
+              setActiveSlideIndex(index);
             }}
             onBeforeSlide={(newIndex) => {
               setActiveSlideIndex(newIndex);
             }}
           />
         </div>
-        <div className='absolute mt-5 transform -translate-x-1/2 -translate-y-1/2 top-1/3 left-1/3 md:ml-36 ml-9 '>
-          <p
-            className={`${fontSize} ${activeSlideIndex === 0 && 'animate-right-to-left'} text-primary-hight-light font-raleway font-bold ${
-              activeSlideIndex === 0 ? 'opacity-100' : 'opacity-0'
-            } transition-opacity duration-300`}
-          >
-            Mulberry International Preschool, Cambodia
-          </p>
-        </div>
 
-        <div className='absolute mt-5 transform -translate-x-1/2 -translate-y-1/2 top-1/3 left-1/3 md:ml-36 ml-9 '>
-          <h2
-            className={`${fontSize} text-primary-hight-light font-raleway font-bold  ${
-              activeSlideIndex === 1 ? 'opacity-100' : 'opacity-0'
-            } transition-opacity duration-300`}
-          >
-            Equip your child with intelligent thinking skills
-          </h2>
-          <div className='flex flex-col items-baseline'>
-            <Link
-              href={'#'}
-              className={`underline md:text-[26px] font-raleway font-bold text-gray-500 ${
-                activeSlideIndex === 1 ? 'opacity-100' : 'opacity-0'
-              } transition-opacity duration-300`}
+        {activeSlideIndex === 0 && (
+          <div className='absolute mt-5 transform -translate-x-1/2 -translate-y-1/2 top-1/3 left-1/3 md:ml-36 ml-9 '>
+            <p
+              className={`${fontSize} text-primary-hight-light font-raleway font-bold animate-left-to-right`}
             >
-              Secure exclusive
-            </Link>
-            <Link
-              href={'#'}
-              className={`underline md:text-[26px] font-raleway font-bold text-gray-500 ${
-                activeSlideIndex === 1 ? 'opacity-100' : 'opacity-0'
-              } transition-opacity duration-300`}
-            >
-              enrollment promotions!
-            </Link>
+              Mulberry International Preschool, Cambodia
+            </p>
           </div>
-        </div>
-
-        <div className='absolute mt-5 transform -translate-x-1/2 -translate-y-1/2 top-1/3 left-1/3 md:ml-36 ml-9 '>
-          <p
-            className={`${fontSize} ${activeSlideIndex === 2 && 'animate-right-to-left'} text-light font-raleway font-bold ${
-              activeSlideIndex === 2 ? 'opacity-100' : 'opacity-0'
-            } transition-opacity duration-300`}
-          >
-            NURTURING THE FUTURE READY CHILD
-          </p>
-          <div
-            className={` text-primary-hight-light font-raleway font-bold ${
-              activeSlideIndex === 2 ? 'opacity-100' : 'opacity-0'
-            } transition-opacity duration-300`}
-          >
-            <button
-              className={`md:mt-4 md:text-[20px] ${activeSlideIndex === 2 && 'animate-left-to-right'} rounded-full bg-siliver text-white px-10 py-3`}
+        )}
+        {activeSlideIndex === 1 && (
+          <div className='absolute mt-5 transform -translate-x-1/2 -translate-y-1/2 top-1/3 left-1/3 md:ml-36 ml-9 '>
+            <h2
+              className={`${fontSize} text-primary-hight-light font-raleway font-bold animate-left-to-right opacity-100 transition-opacity duration-300`}
             >
-              ENROLL NOW
-            </button>
+              Equip your child with intelligent thinking skills
+            </h2>
+            <div className='flex flex-col items-baseline animate-right-to-left opacity-100 transition-opacity duration-300'>
+              <Link
+                href={'#'}
+                className={`underline md:text-[26px] font-raleway font-bold text-gray-500 `}
+              >
+                Secure exclusive
+              </Link>
+              <Link
+                href={'#'}
+                className={`underline md:text-[26px] font-raleway font-bold text-gray-500 `}
+              >
+                enrollment promotions!
+              </Link>
+            </div>
           </div>
-        </div>
+        )}
+        {activeSlideIndex === 2 && (
+          <div className='absolute mt-5 transform -translate-x-1/2 -translate-y-1/2 top-1/3 left-1/3 md:ml-36 ml-9 '>
+            <p
+              className={`${fontSize} animate-right-to-left opacity-100 transition-opacity duration-300 text-light font-raleway font-bold `}
+            >
+              NURTURING THE FUTURE READY CHILD
+            </p>
+            <div
+              className={`text-primary-hight-light font-raleway font-bold animate-left-to-right opacity-100 transition-opacity duration-300`}
+            >
+              <button
+                className={`md:mt-4 md:text-[20px] rounded-full bg-siliver text-white px-10 py-3`}
+              >
+                ENROLL NOW
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
